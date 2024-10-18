9a37465bef28b3d54270df0eb2a8bc11ff3bb719
developer-lfierro743 <lfierro743@cbcsd.org>
2024-10-18 18:30:15 -0500

    pre-developement stage #3.3-fixing the gameloop.java file

--- EaglecraftY/src/com/xojang/eaglecrafty/core/GameLoop.java
+++ EaglecraftY/src/com/xojang/eaglecrafty/core/GameLoop.java
@@ -3,6 +3,38 @@ package com.xojang.eaglecrafty.core;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.glfw.GLFW;
 
-public class GameLoop {
+public abstract class GameLoop {
+    private boolean running;
+    private long window;
 
+    public static void start(long window) {
+        GameLoop loop = new GameLoop() {
+            @Override
+            protected void update() {
+                // Default implementation for update()
+            }
+        };
+        loop.window = window;
+        loop.running = true;
+        loop.run();
+    }
+
+    public void run() {
+        while (running) {
+            update();
+            render();
+        }
+    }
+
+    protected abstract void update();
+
+    private void render() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GLFW.glfwSwapBuffers(window);
+        GLFW.glfwPollEvents();
+    }
+
+    public void stop() {
+        running = false;
+    }
 }
