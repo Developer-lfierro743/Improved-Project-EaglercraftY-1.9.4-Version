432f75cdb000ec2376dd0006829e85fd8f5829f0
developer-lfierro743 <lfierro743@cbcsd.org>
2024-10-18 18:20:23 -0500

    pre-developement stage #3.2-adding java files

---
+++ EaglecraftY/src/com/xojang/eaglecrafty/core/EaglecraftY.java
@@ -0,0 +1,9 @@
+package com.xojang.eaglecrafty.core;
+
+public class EaglecraftY {
+  public static void main(String[] args) {
+    //initialize the game
+    GameLoop gameLoop = new GameLoop();
+    gameLoop.start();
+  }
+}
---
+++ EaglecraftY/src/com/xojang/eaglecrafty/core/GameLoop.java
@@ -0,0 +1,8 @@
+package com.xojang.eaglecrafty.core;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.glfw.GLFW;
+
+public class GameLoop {
+
+}
